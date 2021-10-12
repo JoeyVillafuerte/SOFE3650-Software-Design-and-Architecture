ae033100f59d731ff88c064f15509a6afe9f41b1
JoeyVillafuerte <80644396+JoeyVillafuerte@users.noreply.github.com>
2021-10-12 17:21:03 -0400

    Delete src directory

--- src/Display.java
+++
@@ -1,9 +0,0 @@
-public class Display {
-    public void displayText(Product productInfo) {
-        System.out.print("Scanned Items: ");
-        System.out.print(productInfo.productName + " ");
-        System.out.print("Item Price: ");
-        System.out.print(productInfo.cost);
-        System.out.println();
-    }
-}
